7b791b0d78bae560d0ad3b5b1a8f7ac14e2ccf23
QDung257 <nqdung2507@gmail.com>
2023-10-17 17:18:06 +0700

    chọn ảnh, zoom in out và tạo ảnh xám

---
+++ vdu.py
@@ -0,0 +1,117 @@
+import cv2
+import tkinter as tk
+import tkinter.messagebox
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+image = None
+img = None
+xoay=0
+zoom=1
+dem=0
+def update_image():
+    rotated_img = cv2.warpAffine(original_img, cv2.getRotationMatrix2D((original_img.shape[1] / 2, original_img.shape[0] / 2), current_rotation, 1), (original_img.shape[1], original_img.shape[0]))
+    scaled_img = cv2.resize(rotated_img, None, fx=current_scale, fy=current_scale, interpolation=cv2.INTER_LINEAR)
+    img = cv2.cvtColor(scaled_img, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(img)
+    img = ImageTk.PhotoImage(img)
+    img_label.config(image=img)
+    img_label.image = img
+def select_image():
+    global original_img, current_scale, current_rotation
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        original_img = cv2.imread(file_path)
+        current_scale = 1.0
+        current_rotation = 0
+        rotation_entry.delete(0, "end")  # Xóa nội dung trường nhập góc quay
+        update_image()
+def zoom_in():
+    global current_scale
+    current_scale *= scale_factor
+    update_image()
+
+# Hàm xử lý sự kiện khi nút "Zoom Out" được nhấn
+def zoom_out():
+    global current_scale
+    current_scale /= scale_factor
+    update_image()
+def change_scale():
+    global current_scale
+    scale_text = scale_entry.get()
+    try:
+        scale_value = float(scale_text)
+        if current_choice.get() == 1:
+            current_scale = scale_value
+        elif current_choice.get() == 2:
+            current_scale = 1 / scale_value
+        update_image()
+    except ValueError:
+        pass
+
+    # Hàm xử lý sự kiện khi nút "Xác nhận Góc Quay" được nhấn
+def change_rotation():
+    global current_rotation
+    rotation_text = rotation_entry.get()
+    try:
+        current_rotation = float(rotation_text)
+        update_image()
+    except ValueError:
+        pass
+def anhxam():
+    global image
+    global xam
+    global img
+    # Kiểm tra xem ảnh đã được chọn chưa
+    if (image is None):
+        tkinter.messagebox.showwarning("Cảnh báo", "Bạn chưa chọn ảnh!")
+        return
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('AnhXam',img)
+window = tk.Tk()
+window.title("Zoom In/Out and Rotate Image")
+
+    
+#Khai báo biến toàn cục
+current_scale = 1.0
+current_rotation = 0
+original_img = None
+scale_factor = 1.2
+
+# Tạo nút "Chọn ảnh" và đặt sự kiện khi nút này được nhấn
+select_button = tk.Button(window, text="Chọn ảnh", command=select_image)
+select_button.pack()
+
+# Tạo hộp văn bản để nhập tỷ lệ zoom
+scale_label = tk.Label(window, text="Nhập tỷ lệ zoom:")
+scale_label.pack()
+scale_entry = tk.Entry(window)
+scale_entry.pack()
+scale_confirm_button = tk.Button(window, text="Xác nhận", command=change_scale)
+scale_confirm_button.pack()
+
+# Tạo hộp văn bản để nhập góc quay
+rotation_label = tk.Label(window, text="Nhập góc quay (độ):")
+rotation_label.pack()
+rotation_entry = tk.Entry(window)
+rotation_entry.pack()
+rotation_confirm_button = tk.Button(window, text="Xác nhận Góc Quay", command=change_rotation)
+rotation_confirm_button.pack()
+
+
+# Tạo nút "Zoom In" và "Zoom out"
+current_choice = tk.IntVar()
+current_choice.set(1)
+zoom_in_button = tk.Button(window, text="Zoom In", command=zoom_in)
+zoom_in_button.pack()
+zoom_out_button = tk.Button(window, text="Zoom Out", command=zoom_out)
+zoom_out_button.pack()
+#tạo ảnh xám:
+anhxam_button = tk.Button(window, text="Tạo ảnh xám", command=anhxam)
+anhxam_button.pack()
+# Tạo label để hiển thị ảnh
+img_label = tk.Label(window)
+img_label.pack()
+
+
+window.mainloop()
