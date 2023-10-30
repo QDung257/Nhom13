a79bf62978a1e071148a373a2ca6aaaf7a383055
QDung257 <nqdung2507@gmail.com>
2023-10-30 20:49:51 +0700

    App mô phỏng mở khóa đt

--- mokhoacodevaface.py
+++ mokhoacodevaface.py
@@ -1,23 +1,27 @@
 import sys
 import cv2
 import face_recognition
-import threading
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit, QMainWindow
-from PyQt5.QtCore import QTimer
-from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtCore import QTimer, QDateTime, Qt
+from PyQt5.QtGui import QPixmap, QImage, QFont
+from datetime import datetime, timedelta
 
-# Load the user's face encoding
-user_image = face_recognition.load_image_file("person2.jpg")
+# Ảnh đầu vào để nhận diện
+user_image = face_recognition.load_image_file("person2.jpg") #Ảnh 
 user_face_encoding = face_recognition.face_encodings(user_image)[0]
 
 class FaceUnlockApp(QMainWindow):
     def __init__(self):
-        super(FaceUnlockApp, self).__init__()
-        self.title = "Face Unlock App"
+        super().__init__()
+        self.title = "Mở khóa màn hình"
         self.code = "1234"
         self.entered_code = ""
         self.face_unlock_enabled = False
-        self.recognized_person = "Unknown"
+        self.recognized_person = "Không xác định"
+        self.failed_attempts = 0
+        self.lockout_time = None
+        self.face_unlock_delay = 5  # Đợi 5 giây trước khi quay lại giao diện mặc định
+        self.face_unlock_timer = QTimer(self)
         self.initUI()
 
     def initUI(self):
@@ -26,55 +30,73 @@ class FaceUnlockApp(QMainWindow):
 
         layout = QVBoxLayout()
 
-        self.message_label = QLabel("Enter Code or Enable Face Unlock")
+        # Sử dụng CSS để thay đổi màu sắc cho các thành phần giao diện
+        self.message_label = QLabel("Nhập mã hoặc dùng mở khóa bằng khuôn mặt", self)
+        self.message_label.setStyleSheet("color: blue; font-size: 16px;")
         layout.addWidget(self.message_label)
 
-        self.code_input = QLineEdit()
-        self.code_input.setPlaceholderText("Enter Code")
+        self.code_input = QLineEdit(self)
+        self.code_input.setPlaceholderText("Nhập mật khẩu")
+        self.code_input.setStyleSheet("background-color: white; font-size: 16px;")
         layout.addWidget(self.code_input)
 
-        code_button = QPushButton("Unlock with Code")
+        code_button = QPushButton("Nhập mật khẩu", self)
+        code_button.setStyleSheet("background-color: white; color: black; font-size: 16px;")
         code_button.clicked.connect(self.unlock_with_code)
         layout.addWidget(code_button)
 
-        face_unlock_button = QPushButton("Toggle Face Unlock")
+        face_unlock_button = QPushButton("Chuyển đổi mở khóa khuôn mặt", self)
+        face_unlock_button.setStyleSheet("background-color: white; color: black; font-size: 16px;")
         face_unlock_button.clicked.connect(self.toggle_face_unlock)
         layout.addWidget(face_unlock_button)
 
         self.image_label = QLabel(self)
         layout.addWidget(self.image_label)
 
-        # Additional label for displaying recognized person's info
         self.info_label = QLabel(self)
+        self.info_label.setStyleSheet("color: red; font-size: 16px;")
         layout.addWidget(self.info_label)
 
         self.central_widget.setLayout(layout)
 
-        # Initialize the camera capture
         self.cap = cv2.VideoCapture(0)
 
-        # Create a timer to continuously update the camera image
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_camera_image)
-        self.timer.start(100)  # Update every 100 milliseconds
+        self.timer.start(100)# quét liên tục cho đến khi nhận diện được (100ms)
+
+        self.face_unlock_timer.timeout.connect(self.return_to_default_interface)
 
     def unlock_with_code(self):
         if self.face_unlock_enabled:
-            self.message_label.setText("Face Unlock is enabled. Please use Face Unlock.")
+            self.message_label.setText("Mở khóa khuôn mặt đang được bật. Vui lòng dùng khuôn mặt để mở khóa.")
         else:
-            if self.code_input.text() == self.code:
-                self.message_label.setText("Unlock Successful!")
-                self.entered_code = ""
+            if self.lockout_time is not None and datetime.now() < self.lockout_time:
+                remaining_time = self.lockout_time - datetime.now()
+                self.message_label.setText(f"Thử lại sau {remaining_time.seconds} giây.")
             else:
-                self.message_label.setText("Unlock Failed. Please try again.")
-            self.code_input.clear()
+                if self.code_input.text() == self.code:
+                    self.message_label.setText("Mở khóa thành công!")
+                    self.entered_code = ""
+                    self.failed_attempts = 0
+                else:
+                    self.failed_attempts += 1
+                    if self.failed_attempts >= 5:
+                        self.lockout_time = datetime.now() + timedelta(minutes=1)  # Khóa trong 1 phút
+                    self.message_label.setText("Mở khóa thất bại! Vui lòng thử lại.")
+                self.code_input.clear()
 
     def toggle_face_unlock(self):
-        self.face_unlock_enabled = not self.face_unlock_enabled
+        if self.face_unlock_timer.isActive():
+            self.face_unlock_timer.stop()
         if self.face_unlock_enabled:
-            self.message_label.setText("Face Unlock is enabled.")
+            self.message_label.setText("Nhập mã hoặc mở bằng khuôn mặt")
         else:
-            self.message_label.setText("Enter Code or Enable Face Unlock")
+            self.message_label.setText("Mở khóa bằng khuôn mặt đang được bật.")
+            
+            self.face_unlock_timer.start(self.face_unlock_delay * 1000)
+
+        self.face_unlock_enabled = not self.face_unlock_enabled
 
     def update_camera_image(self):
         if self.cap.isOpened():
@@ -83,12 +105,14 @@ class FaceUnlockApp(QMainWindow):
                 if self.face_unlock_enabled:
                     face_locations = face_recognition.face_locations(frame)
                     face_encodings = face_recognition.face_encodings(frame, face_locations)
-                    self.recognized_person = "Unknown"  # Default to "Unknown" unless a match is found
+                    self.recognized_person = "Không xác định được"
                     for face_encoding in face_encodings:
                         matches = face_recognition.compare_faces([user_face_encoding], face_encoding)
                         if True in matches:
-                            self.recognized_person = "User: QD"  # Replace with the actual person's info
+                            self.recognized_person = "User: Quang Dũng"
                             self.message_label.setText("Mở khóa thành công!")
+                            if self.face_unlock_timer.isActive():
+                                self.face_unlock_timer.stop()
                             break
 
                 height, width, channel = frame.shape
@@ -98,8 +122,13 @@ class FaceUnlockApp(QMainWindow):
                 pixmap = QPixmap.fromImage(qImg)
                 self.image_label.setPixmap(pixmap)
 
-                # Update the recognized person's info
+                # Cập nhật thông tin người được công nhận
                 self.info_label.setText(self.recognized_person)
+
+    def return_to_default_interface(self):
+        self.face_unlock_enabled = False
+        self.message_label.setText("Nhập mã hoặc dùng mở bằng khuôn mặt")
+        self.face_unlock_timer.stop()
 
     def closeEvent(self, event):
         self.cap.release()
